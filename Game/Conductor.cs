5e228ef2389f2004f77468e7a72c5fe8a75070be
Swippy <58222035+ZackSlipper@users.noreply.github.com>
2026-01-01 14:53:10 +0200

    Updated godot and removed character from level

---
+++ Game/Conductor.cs
@@ -0,0 +1,8 @@
+using Godot;
+
+namespace RhythmMania.Game;
+
+public partial class Conductor : Node
+{
+	
+}
---
+++ Game/NoteTrack.cs
@@ -0,0 +1,8 @@
+using Godot;
+
+namespace RhythmMania.Game;
+
+public partial class NoteTrack : Node
+{
+	[Export] private PackedScene notePrefab;
+}
